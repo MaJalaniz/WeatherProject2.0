bd91339097ace20be8fdb1843418becbf7a14a5f
Matthew Alaniz <alaniz.matthew07@gmail.com>
2019-10-22 17:07:00 -0500

    Changing Fahrenheit to Celsius temps

--- js/weatherJS.js
+++ js/weatherJS.js
@@ -4,7 +4,8 @@ window.addEventListener('load', () => {
     let temperatureDescription = document.querySelector('.temperature-description');
     let temperatureDegree = document.querySelector('.temperature-degree');
     let timeZone = document.querySelector('.location-timezone');
-
+    let temperatureSection = document.querySelector('.temperature')
+    const temperatureSpan = document.querySelector('.temperature span');
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position =>{
@@ -12,6 +13,7 @@ window.addEventListener('load', () => {
             lat = position.coords.latitude;
 
             const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const apiKey = `your api key here!`;
 
             fetch(apiKey)
         .then(data => {
@@ -24,8 +26,21 @@ window.addEventListener('load', () => {
             temperatureDegree.textContent = temperature;
             temperatureDescription.textContent = summary;
             timeZone.textContent = data.timezone;
+
+            let celsius = (temperature - 32) * (5 / 9);
+
             setIcons(icon, document.querySelector('.icon'))
-        })
+
+            temperatureSection.addEventListener("click", () => {
+                if(temperatureSpan.textContent === "F"){
+                    temperatureSpan.textContent = "C";
+                    temperatureDegree.textContent = Math.floor(celsius);
+                }else{
+                    temperatureSpan.textContent = "F";
+                    temperatureDegree.textContent = temperature;
+                }
+            })
+        });
         });
         
     }
