d1088624dac4403bd7c5e41d96153d90af62a3f0
Matthew Alaniz <alaniz.matthew07@gmail.com>
2019-10-22 16:15:33 -0500

    Getting data to display on browser window

--- js/weatherJS.js
+++ js/weatherJS.js
@@ -1,6 +1,10 @@
 window.addEventListener('load', () => {
     let long;
     let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let timeZone = document.querySelector('.location-timezone');
+
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position =>{
@@ -8,7 +12,6 @@ window.addEventListener('load', () => {
             lat = position.coords.latitude;
 
             const proxy = 'https://cors-anywhere.herokuapp.com/';
-           
 
             fetch(apiKey)
         .then(data => {
@@ -17,6 +20,10 @@ window.addEventListener('load', () => {
         .then(data => {
             console.log(data);
             const { temperature, summary } = data.currently;
+
+            temperatureDegree.textContent = temperature;
+            temperatureDescription.textContent = summary;
+            timeZone.textContent = data.timezone;
         })
         });
         
